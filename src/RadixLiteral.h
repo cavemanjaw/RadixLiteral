918b9a5684d0642c146aee1c92ab6e4fa8f07931
cavemanjaw_ubuntu <cavemanjaw@op.pl>
2020-11-12 20:49:27 +0100

    Switching to <cstddef> instead <type_traits> for std::size_t definition

--- src/RadixLiteral.h
+++ src/RadixLiteral.h
@@ -15,8 +15,10 @@
 // Reimplement type_traits for smaller binaries size when this header is included
 // (probably true, at least in a portion of cases)
 // TODO: std::size_t depends on it
-#include <type_traits> // for enable_if, TODO: Any other option to disambiguate the template function call?
+//#include <type_traits> // for enable_if, TODO: Any other option to disambiguate the template function call?
 //#define _b(numeral_system)
+// For std::size_t, probably the most lightweight header that does not declare any function
+#include <cstddef>
 
 namespace RadixLiteral
 {
