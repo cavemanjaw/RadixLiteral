3c22decc5397e90a167d07dc8c709bfc3e455dc6
cavemanjaw_ubuntu <cavemanjaw@op.pl>
2020-11-16 17:09:32 +0100

    Adding some tests - static_assert invocations

--- test/RadixLiteral.cpp
+++ test/RadixLiteral.cpp
@@ -16,14 +16,34 @@
 
 #define NUM_OF_TESTS 6
 
-OPERATOR_LITERAL(3);
-OPERATOR_LITERAL(5);
+// for loop equivalent for printing the n number of lines of OPERATOR_LITERAL(n);
 OPERATOR_LITERAL(2);
+OPERATOR_LITERAL(3);
+OPERATOR_LITERAL(4);
+OPERATOR_LITERAL(5);
+OPERATOR_LITERAL(6);
+OPERATOR_LITERAL(7);
+OPERATOR_LITERAL(8);
+OPERATOR_LITERAL(9);
+OPERATOR_LITERAL(10);
+OPERATOR_LITERAL(11);
+OPERATOR_LITERAL(12);
+OPERATOR_LITERAL(13);
+OPERATOR_LITERAL(14);
 OPERATOR_LITERAL(15);
+OPERATOR_LITERAL(16);
+OPERATOR_LITERAL(17);
+OPERATOR_LITERAL(18);
+OPERATOR_LITERAL(19);
+OPERATOR_LITERAL(20);
+
+// TODO: Stress and check the boundary conditions OPERATOR_LITERAL(INT_MAX); and so on...
 
 //STATIC, COMPILE-TIME TESTS:
 static_assert(111_b2 == 7, "testBinary failed! 111_b2 != 7");
 static_assert(231_b3 == 28, "testTernary failed! 231_b3 != 28");
+static_assert(323_b4 == 59, "323_b4 != 59");
+static_assert(324_b5 == 89, "324_b5 != 89");
 static_assert(111_b15 == 241, "test15BaseOnlyDigits failed! 111_b15 != 241");
 static_assert("ba1"_b15 == 2626, "test15BaseLowerCase failed! \"ba1\"_b15 != 2626");
 static_assert("BA1"_b15 == 2626, "test15BaseUpperCase failed! \"BA1\"_b15 != 2626");
